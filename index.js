8287703669877a2e15db10dd11d991cc12dc0fb1
imccunn <ianmcunn@gmail.com>
2017-09-09 23:56:41 -0700

    webpack

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var express = require('express');
-
 const app = express();
 
 app.use(express.static('./'));
 
-app.get('/testing', (req, res) => {
-  res.status(200).send({msg: 'hello'});
-});
-
 app.listen(8080, () => {
   console.log('Server listening on 8080');
 });
-
